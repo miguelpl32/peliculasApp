b8080eaddd0721756957b8b6e30fd33b81719ff7
Miguel-Pariente <miguelpl32@hotmail.com>
2020-10-25 19:59:19 +0100

    Creacion Pipe no imagen

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { RatingModule } from 'ng-starrating';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 
 
@@ -18,7 +19,8 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
   imports: [
     CommonModule,
     RouterModule,
-    RatingModule
+    RatingModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
