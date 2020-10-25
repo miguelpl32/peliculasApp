c30ed19f507681313050f8239cd3e739e161b265
Miguel-Pariente <miguelpl32@hotmail.com>
2020-10-25 18:04:24 +0100

    Creacion componente Slides, carga imagenes, descripcion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,5 @@ import { PeliculasService } from './services/peliculas.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private peliculasService: PeliculasService) {
-    this.peliculasService.getCartelera().subscribe(resp => {
-      console.log(resp);
-    });
-  }
+
 }
